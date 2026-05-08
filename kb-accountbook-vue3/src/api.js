82c6915574eed52292024dedb7b3bd003a19dc84
arbreol <arbreol0@gmail.com>
2026-05-08 10:26:33 +0900

    All new

---
+++ kb-accountbook-vue3/src/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL || 'https://kb-figma-ak0v.onrender.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
