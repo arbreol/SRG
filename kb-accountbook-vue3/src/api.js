07f47abd9a32f4a6f9514094e59c30165cb74498
arbreol <arbreol0@gmail.com>
2026-05-08 10:26:14 +0900

    Remoce Cache

--- kb-accountbook-vue3/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || 'https://kb-figma-ak0v.onrender.com',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
